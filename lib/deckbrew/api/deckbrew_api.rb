092b63384ef2bd36fd6aadbed76a35b8fe4576e8
Alex Jarvis <alxjrvs@gmail.com>
2015-11-09 22:16:51 -0500

    I guess it is called ‘ruby’ now? Idk.

--- lib/deckbrew/api/deckbrew_api.rb
+++ lib/deckbrew/api/deckbrew_api.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 module Deckbrew
   module API
     class DeckbrewAPI
--- lib/deckbrew/ruby.rb
+++ lib/deckbrew/ruby.rb
@@ -6,5 +6,4 @@ require "deckbrew/magic_metadata"
 require "deckbrew/magic_set"
 
 module Deckbrew
-  binding.pry
 end
