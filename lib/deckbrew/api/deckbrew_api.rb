4a06b2721116da874150a19ff10d34c545daca69
Alex Jarvis <alxjrvs@gmail.com>
2015-11-09 22:14:06 -0500

    Moving under api namespace

--- lib/deckbrew/api/deckbrew_api.rb
+++ lib/deckbrew/api/deckbrew_api.rb
@@ -1,50 +1,52 @@
 module Deckbrew
-  class DeckbrewAPI
-    include Httparty
-    base_uri 'https://api.deckbrew.com'
+  module API
+    class DeckbrewAPI
+      include Httparty
+      base_uri 'https://api.deckbrew.com'
 
-    CARDS = '/mtg/cards'
-    SETS = '/mtg/sets'
-    TYPES = '/mtg/types'
-    SUPERTYPES = '/mtg/supertypes'
-    SUBTYPES = '/mtg/subtypes'
-    COLORS = '/mtg/colors'
+      CARDS = '/mtg/cards'
+      SETS = '/mtg/sets'
+      TYPES = '/mtg/types'
+      SUPERTYPES = '/mtg/supertypes'
+      SUBTYPES = '/mtg/subtypes'
+      COLORS = '/mtg/colors'
 
-    def self.get_all_cards
-      get CARDS
-    end
+      def self.get_all_cards
+        get CARDS
+      end
 
-    def self.get_card(id)
-      get "#{CARDS}/#{id}"
-    end
+      def self.get_card(id)
+        get "#{CARDS}/#{id}"
+      end
 
-    def self.get_cards_filter()
-      queries = {}
-      get CARDS, queries
-    end
+      def self.get_cards_filter()
+        queries = {}
+        get CARDS, queries
+      end
 
-    def self.get_all_sets
-      get SETS
-    end
+      def self.get_all_sets
+        get SETS
+      end
 
-    def self.get_set(id)
-      get "#{SETS}/#{id}"
-    end
+      def self.get_set(id)
+        get "#{SETS}/#{id}"
+      end
 
-    def self.get_types
-      get TYPES
-    end
+      def self.get_types
+        get TYPES
+      end
 
-    def self.get_supertypes
-      get SUPERTYPES
-    end
+      def self.get_supertypes
+        get SUPERTYPES
+      end
 
-    def self.get_subtypes
-      get SUBTYPES
-    end
+      def self.get_subtypes
+        get SUBTYPES
+      end
 
-    def self.get_colors
-      get COLORS
+      def self.get_colors
+        get COLORS
+      end
     end
   end
 end
